da63e5320faa66b2a4aff47b251ff9435fb4ad0a
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2025-11-27 16:58:06 +0100

    Añadir ejercicio 1 de practica y resultado (notas.txt)

--- Sistemas Informaticos/UD3/escribir_archivo.sh
+++ Sistemas Informaticos/UD3/escribir_archivo.sh
@@ -2,4 +2,5 @@ nombre_archivo=$1
 palabra1=$2
 palabra2=$3
 palabra3=$4
-echo(palabra3 \n palabra2 \n palabra1 >> nombre_archivo)
+palabra4=$5
+echo -e "$palabra4\n$palabra3\n$palabra2\n$palabra1" >> "$nombre_archivo"
