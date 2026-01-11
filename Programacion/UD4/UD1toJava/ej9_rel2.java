cdd84777de8ffea7b0bb7263e75e0e7d71696cbf
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-01-11 18:41:24 +0100

    Complete ej9_rel2

--- Programacion/UD4/UD1toJava/ej9_rel2.java
+++ Programacion/UD4/UD1toJava/ej9_rel2.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
 public class ej9_rel2 {
 
-    
+    public static void main(String[] args) {
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.print("Introduzca el número de caramelos:");
+        int caramelos = scanner1.nextInt();
+        System.out.print("¿Cuántos niños/as hay?");
+        int ninos = scanner1.nextInt();
+        int carameloPorNino = caramelos / ninos;
+        int resto = caramelos % ninos;
+        System.out.println("Cada niño/a recibirá " + carameloPorNino + " caramelos, y sobrarán " + resto + " caramelos.");
+        scanner1.close();
+    }
 }
