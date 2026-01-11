668a6316c7035402cafddaafed6d9fbfc79a2323
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-01-11 19:15:50 +0100

    Add placeholder for ej7 (finished but doesnt work like intended)

--- Programacion/UD4/UD1toJava/rel3_ej7.java
+++ Programacion/UD4/UD1toJava/rel3_ej7.java
@@ -3,11 +3,28 @@ import java.util.Scanner;
 public class rel3_ej7 {
     public static void main(String[] args) {
         Scanner scanner1 = new Scanner(System.in);
-        System.out.print("Ingrese su peso actual: ");
-        int peso = scanner1.nextInt();
-        double relacionPeso = 0.165;
-        double pesoLuna = peso * relacionPeso;
-        System.out.println("Su peso en la Luna sería de " + pesoLuna + " kilos!");
+        System.out.print("Introduzca un número:");
+        double numero1 = scanner1.nextDouble();
+        System.out.print("Introduzca otro número:");
+        double numero2 = scanner1.nextDouble();
+        System.out.print("Introduce un operante:");
+        String operante = scanner1.next();
+        double resultado = 0;
+        if (operante == "+") {
+            resultado = numero1 + numero2;
+        } else if (operante == "-") {
+            resultado = numero1 - numero2;
+        } else if (operante == "*" || operante == "x") {
+            resultado = numero1 * numero2;
+        } else if (operante == "/") {
+            resultado = numero1 / numero2;
+        } else if (operante == "%") {
+            resultado = numero1 % numero2;
+        } else {
+            System.out.print("No se ha introducido un operante válido.");
+        }
+        System.out.println("El resultado de la operación es " + resultado + ".");
         scanner1.close();
     }
+    // DOES NOT WORK
 }
