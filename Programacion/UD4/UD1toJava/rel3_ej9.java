423308eacb41bdfbd1310f3d3aaed01bdff4124e
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-01-11 19:29:35 +0100

    Complete ej9

--- Programacion/UD4/UD1toJava/rel3_ej9.java
+++ Programacion/UD4/UD1toJava/rel3_ej9.java
@@ -3,11 +3,19 @@ import java.util.Scanner;
 public class rel3_ej9 {
     public static void main(String[] args) {
         Scanner scanner1 = new Scanner(System.in);
-        System.out.print("Ingrese su peso actual: ");
-        int peso = scanner1.nextInt();
-        double relacionPeso = 0.165;
-        double pesoLuna = peso * relacionPeso;
-        System.out.println("Su peso en la Luna sería de " + pesoLuna + " kilos!");
+        System.out.print("Introduzca un número de dos cifras:");
+        int numero = scanner1.nextInt();
+        int digito1 = 0;
+        int digito2 = 0;
+        int sumaDigitos = 0;
+        if ((numero >= 10 && numero < 100) || (numero <= -10 && numero > -100)) {
+            digito1 = numero / 10;
+            digito2 = numero % 10;
+            sumaDigitos = digito1 + digito2;
+            System.out.print("La suma de los dos digitos es " + sumaDigitos + ".");
+        } else {
+            System.out.print("El número introducido no es de dos cifras!");
+        }
         scanner1.close();
     }
 }
