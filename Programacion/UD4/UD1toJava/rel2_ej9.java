7fbf5131534221ea5b999f83ca4ee0dcfa7a4002
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-01-12 15:48:14 +0100

    Improve rel2_ej9

--- Programacion/UD4/UD1toJava/rel2_ej9.java
+++ Programacion/UD4/UD1toJava/rel2_ej9.java
@@ -2,14 +2,22 @@ import java.util.Scanner;
 public class rel2_ej9 {
 
     public static void main(String[] args) {
+        // declaracion de variables
+        int caramelos;
+        int ninos;
+        int carameloPorNino;
+        int resto;
         Scanner scanner1 = new Scanner(System.in);
+        // pide al usuario el numero de caramelos y ninos
         System.out.print("Introduzca el número de caramelos:");
-        int caramelos = scanner1.nextInt();
+        caramelos = scanner1.nextInt();
         System.out.print("¿Cuántos niños/as hay?");
-        int ninos = scanner1.nextInt();
-        int carameloPorNino = caramelos / ninos;
-        int resto = caramelos % ninos;
-        System.out.println("Cada niño/a recibirá " + carameloPorNino + " caramelos, y sobrarán " + resto + " caramelos.");
+        ninos = scanner1.nextInt();
+        // se cierra el escaner
         scanner1.close();
+        // se calcula el numero de caramelos per capita y el resto
+        carameloPorNino = caramelos / ninos;
+        resto = caramelos % ninos;
+        System.out.println("Cada niño/a recibirá " + carameloPorNino + " caramelos, y sobrarán " + resto + " caramelos.");
     }
 }
