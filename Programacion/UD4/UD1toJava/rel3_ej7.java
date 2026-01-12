9c741d23b5e95eca6725410e67bb1d42f0c92d96
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-01-12 15:58:12 +0100

    Fix and improve rel3_ej7

--- Programacion/UD4/UD1toJava/rel3_ej7.java
+++ Programacion/UD4/UD1toJava/rel3_ej7.java
@@ -2,29 +2,35 @@ import java.util.Scanner;
 
 public class rel3_ej7 {
     public static void main(String[] args) {
-        Scanner scanner1 = new Scanner(System.in);
-        System.out.print("Introduzca un número:");
-        double numero1 = scanner1.nextDouble();
-        System.out.print("Introduzca otro número:");
-        double numero2 = scanner1.nextDouble();
-        System.out.print("Introduce un operante:");
-        String operante = scanner1.next();
+        // inicializa variables
+        double numero1;
+        double numero2;
+        String operante;
         double resultado = 0;
-        if (operante == "+") {
+        Scanner scanner1 = new Scanner(System.in);
+        // pide los numeros y el operante al usuario
+        System.out.print("Introduzca un número:");
+        numero1 = scanner1.nextDouble();
+        System.out.print("Introduzca otro número:");
+        numero2 = scanner1.nextDouble();
+        System.out.print("Introduce un operante:");
+        operante = scanner1.next();
+        scanner1.close();
+        // condiciones que comprueban que operante se ha introducido
+        if (operante.equals("+")) {
             resultado = numero1 + numero2;
-        } else if (operante == "-") {
+        } else if (operante.equals("-")) {
             resultado = numero1 - numero2;
-        } else if (operante == "*" || operante == "x") {
+        } else if (operante.equals("*") || operante.equals("x")) {
             resultado = numero1 * numero2;
-        } else if (operante == "/") {
+        } else if (operante.equals("/")) {
             resultado = numero1 / numero2;
-        } else if (operante == "%") {
+        } else if (operante.equals("%")) {
             resultado = numero1 % numero2;
         } else {
             System.out.print("No se ha introducido un operante válido.");
         }
+        // presenta el resultado al usuario
         System.out.println("El resultado de la operación es " + resultado + ".");
-        scanner1.close();
     }
-    // DOES NOT WORK
 }
