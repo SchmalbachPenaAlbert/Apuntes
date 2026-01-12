66194a8eb222f5fe6da07d518e59c80481081888
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-01-12 15:39:11 +0100

    Improve rel2_ej5

--- Programacion/UD4/UD1toJava/rel2_ej5.java
+++ Programacion/UD4/UD1toJava/rel2_ej5.java
@@ -2,12 +2,20 @@ import java.util.Scanner;
 
 public class rel2_ej5 {
     public static void main(String[] args) {
-        Scanner scanner1 = new Scanner(System.in);
-        System.out.print("Ingrese su peso actual: ");
-        int peso = scanner1.nextInt();
+        // declara variables
         double relacionPeso = 0.165;
-        double pesoLuna = peso * relacionPeso;
-        System.out.println("Su peso en la Luna sería de " + pesoLuna + " kilos!");
-        scanner1.close();
+        double peso;
+        double pesoLuna;
+        // crea un nuevo escaner
+        Scanner escaner = new Scanner(System.in);
+        // pide el peso al usuario
+        System.out.print("Ingrese su peso actual:");
+        peso = escaner.nextDouble();
+        // cierra el escaner
+        escaner.close();
+        // calcula el peso en la luna segun los dos datos Double
+        pesoLuna = peso * relacionPeso;
+        // se muestra el resultado
+        System.out.printf("Su peso en la Luna sería de %.2f kilos!\n", pesoLuna);
     }
 }
