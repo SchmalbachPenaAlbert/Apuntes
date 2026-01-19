ddc735d7c627fc860821113ac976eea7efc9f353
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-01-19 16:14:02 +0100

    Add exercises 9 and 10

---
+++ Programacion/UD4/UD2toJava/rel1_ej10.java
@@ -0,0 +1,19 @@
+package Programacion.UD4.UD2toJava;
+import java.util.ArrayList;
+
+public class rel1_ej10 {
+    public static void main(String[] args) {
+        ArrayList<Integer> pila = new ArrayList<Integer>();
+        // añade valores al array
+        pila.add(10);
+        pila.add(20);
+        pila.add(30);
+        System.out.println(pila);
+        System.out.println(pila.remove(2));
+        System.out.println(pila);
+        System.out.println(pila.remove(1));
+        System.out.println(pila);
+        System.out.println(pila.remove(0));
+        System.out.println(pila);
+    }
+}
---
+++ Programacion/UD4/UD2toJava/rel1_ej9.java
@@ -0,0 +1,19 @@
+package Programacion.UD4.UD2toJava;
+import java.util.ArrayList;
+
+public class rel1_ej9 {
+    public static void main(String[] args) {
+        ArrayList<String> cola = new ArrayList<String>();
+        // añade valores al array
+        cola.add("cliente1");
+        cola.add("cliente2");
+        cola.add("cliente3");
+        System.out.println(cola);
+        System.out.println(cola.remove(0));
+        System.out.println(cola);
+        System.out.println(cola.remove(0));
+        System.out.println(cola);
+        System.out.println(cola.remove(0));
+        System.out.println(cola);
+    }
+}
