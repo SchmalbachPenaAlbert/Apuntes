0fdf00f20112f63cd0cc79865170f4c1b3127d82
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-01-19 15:36:21 +0100

    Add files from UD2 translated to Java

---
+++ Programacion/UD4/UD2toJava/rel1_ej1.java
@@ -0,0 +1,14 @@
+package Programacion.UD4.UD2toJava;
+import java.util.ArrayList;
+
+public class rel1_ej1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> numeros = new ArrayList<Integer>();
+        numeros.add(5);
+        numeros.add(10);
+        numeros.add(15);
+        numeros.add(20);
+        System.out.println(numeros.get(0));
+        System.out.println(numeros.get(3));
+    }
+}
---
+++ Programacion/UD4/UD2toJava/rel1_ej2.java
@@ -0,0 +1,12 @@
+package Programacion.UD4.UD2toJava;
+import java.util.ArrayList;
+
+public class rel1_ej2 {
+    public static void main(String[] args) {
+        ArrayList<String> colores = new ArrayList<String>();
+        colores.add("rojo");
+        colores.add("verde");
+        colores.add("azul");
+        System.out.println(colores);
+    }
+}
