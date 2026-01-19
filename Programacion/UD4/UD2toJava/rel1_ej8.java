8f14a15b73c7625666676b557ad7c512c4f32012
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-01-19 16:02:31 +0100

    Fix exercise 8's comments

--- Programacion/UD4/UD2toJava/rel1_ej8.java
+++ Programacion/UD4/UD2toJava/rel1_ej8.java
@@ -4,14 +4,17 @@ import java.util.ArrayList;
 public class rel1_ej8 {
     public static void main(String[] args) {
         ArrayList<Integer> numero = new ArrayList<Integer>();
+        // añade valores al array
         numero.add(100);
         numero.add(200);
         numero.add(300);
+        // crea un array temporal
         ArrayList<Integer> temp = new ArrayList<Integer>();
         // añade el elemento en posición 0 al nuevo array
         temp.add(numero.get(0));
-        // se copia el elemento de la posicion 2 a la posicion 0
+        // copia el elemento de la posicion 2 a la posicion 0
         numero.set(0, numero.get(2));
+        // copia el elemento del array temporal a la posicion 2 del array principal
         numero.set(2, temp.get(0));
         System.out.println(numero);
     }
