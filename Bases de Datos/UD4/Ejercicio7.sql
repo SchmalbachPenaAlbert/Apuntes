2dd17b42e7ddfe8b12629cd81748697789cfd058
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-01-26 20:20:29 +0100

    Import the rest of exercise 7

--- Bases de Datos/UD4/Ejercicio7.sql
+++ Bases de Datos/UD4/Ejercicio7.sql
@@ -75,7 +75,7 @@ INSERT INTO itinerario (id_procesion, punto, hora_paso) VALUES
 (4, 'Abadía del Sacromonte', '21:30:00');
 
 -- 1. Listar todos los nombres de las procesiones.
-
+select nombres from procesion;
 
 -- 2. Mostrar los barrios donde se realizan las procesiones, sin repetir.
 
@@ -87,3 +87,114 @@ INSERT INTO itinerario (id_procesion, punto, hora_paso) VALUES
 
 
 -- 5. Mostrar el nombre de las hermandades que tienen más de 100 costaleros.
+
+
+-- 6. Listar las procesiones que ocurren el Viernes Santo.
+
+
+-- 7. Mostrar los puntos de paso de las procesiones que pasan por 'Catedral'.
+
+
+-- 8. Obtener las procesiones que tienen lugar en el barrio 'Centro'.
+
+
+-- 9. Listar los estilos de pasos únicos.
+
+
+-- 10. Mostrar los nombres de los pasos que pesan más de 1900 kg.
+
+
+-- 11. Listar los pasos cuyo peso esté entre 1800 y 2200 kg.
+
+
+-- 12. Mostrar las hermandades fundadas antes del año 1600.
+
+
+-- 13. Seleccionar las procesiones que comienzan después de las 19:00 y están en el barrio 'Realejo'.
+
+
+-- 14. Listar las hermandades que tienen más de 80 costaleros o fueron fundadas después de 1900.
+
+
+-- 15. Mostrar las procesiones que no se realizan el Domingo de Ramos.
+
+
+-- 16. Mostrar todas las procesiones ordenadas por la hora de inicio en orden ascendente.
+
+
+-- 17. Listar las hermandades ordenadas por antigüedad de forma descendente.
+
+
+-- 18. Seleccionar los pasos ordenados por peso y, en caso de empate, por estilo alfabético.
+
+
+-- 20. Mostrar los puntos de paso de las procesiones ordenados por hora de paso en orden descendente.
+
+
+-- 21. Contar cuántas procesiones hay en total.
+
+
+-- 22. Obtener el peso promedio de todos los pasos.
+
+
+-- 23. Calcular el número máximo de costaleros en una hermandad.
+
+
+-- 24. Obtener el número de procesiones que se realizan en el barrio 'Albaicín'.
+
+
+-- 25. Sumar el peso de todos los pasos.
+
+
+-- 26. Agrupar las procesiones por barrio y contar cuántas procesiones hay en cada uno.
+
+
+-- 27. Calcular el peso total de los pasos agrupados por estilo.
+
+
+-- 28. Mostrar las hermandades agrupadas por número de costaleros, filtrando aquellas con más de 100 costaleros.
+
+
+-- 29. Obtener el número de procesiones que tienen puntos de paso después de las 20:00.
+
+
+-- 30. Agrupar las hermandades por antigüedad y contar cuántas hay en cada rango.
+
+
+-- 31. Concatenar el nombre y barrio de cada procesión en un único campo, separados por un guion.
+
+
+-- 32. Mostrar las procesiones cuyos nombres contienen más de 10 caracteres.
+
+
+-- 33. Seleccionar los nombres de las procesiones que contienen la palabra 'Soledad'.
+
+
+-- 34. Mostrar las hermandades ordenadas por el número de costaleros en orden ascendente.
+
+
+-- 35. Listar los nombres de los pasos en los que la palabra 'Virgen' no aparece.
+
+
+-- 36. Calcular la diferencia entre el peso máximo y mínimo de los pasos.
+
+
+-- 37. Mostrar solo las procesiones cuyo nombre no empieza con 'La'.
+
+
+-- 38. Formatear la hora de inicio de las procesiones en formato de 12 horas con AM/PM.
+
+
+-- 39. Mostrar solo la hora (sin minutos ni segundos) del paso por cada punto del itinerario.
+
+
+-- 40. Seleccionar las procesiones que tienen su inicio en las próximas 6 horas a partir de la fecha y hora actual.
+
+
+-- 41. Redondear el peso de los pasos a la centena más cercana.
+
+
+-- 42. Mostrar el peso de los pasos dividido entre 1000, con dos decimales.
+
+
+-- 43. Generar un número aleatorio entre 1 y 100.
