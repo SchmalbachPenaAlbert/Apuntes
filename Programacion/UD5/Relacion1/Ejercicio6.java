4ca67d38dd897cc15bb41a035128397089ad96ac
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-02-12 12:54:55 +0100

    Add main functionality without bug fixing

--- Programacion/UD5/Relacion1/Ejercicio6.java
+++ Programacion/UD5/Relacion1/Ejercicio6.java
@@ -1,9 +1,8 @@
-
-import java.util.Date;
-
 public class Ejercicio6 {
     public static void main(String[] args) {
         Reloj reloj1 = new Reloj(12, 38, 23);
+        reloj1.sumarMinutos(54);
+        reloj1.restarSegundos(28);
         System.out.println(reloj1);
     }
 }
@@ -16,25 +15,17 @@ class Reloj {
         this.minuto = minuto;
         this.segundo = segundo;
     }
-    int sumarMinutos(minutos) {
-        int nuevoMinuto;
-        //TODO
-        return nuevoMinuto;
+    void sumarMinutos(int minutos) {
+        minuto += minutos;
     }
-    int restarMinutos(minutos) {
-        int nuevoMinuto;
-        //TODO
-        return nuevoMinuto;
+    void restarMinutos(int minutos) {
+        minuto -= minutos;
     }
-    int sumarSegundos(segundos) {
-        int nuevoSegundo;
-        //TODO
-        return nuevoSegundo;
+    void sumarSegundos(int segundos) {
+        segundo += segundos;
     }
-    int restarSegundos(segundos) {
-        int nuevoSegundo;
-        //TODO
-        return nuevoSegundo;
+    void restarSegundos(int segundos) {
+        segundo -= segundos;
     }
 
     @Override
