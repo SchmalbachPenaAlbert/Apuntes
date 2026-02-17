4e6218a4c76aa57a286d0556039863357daf09d8
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-02-17 15:54:13 +0100

    Add Potencia function

--- Sistemas Informaticos/UD5/Ejercicios2.sh
+++ Sistemas Informaticos/UD5/Ejercicios2.sh
@@ -5,3 +5,33 @@ multiplicar() {
     resultado=$( echo "$1 * $2" | bc -l)
     echo $resultado
 }
+
+potencia() {
+    base=$1
+    exponente=$2
+    i=0
+    while [ $i -lt $exponente ]
+    do
+        resultado=$( echo "$resultado * $base" | bc -l)
+        ((i++))
+    done
+    echo $resultado
+}
+
+echo "Ej 4:"
+multiplicar $1 $2
+echo "Ej 5:"
+potencia $1 $2
+echo "Ej 7:"
+
+echo "Ej 8:"
+
+echo "Ej 9:"
+
+echo "Ej 10:"
+
+echo "Ej 11:"
+
+echo "Ej 12:"
+
+echo "Ej 13:"
