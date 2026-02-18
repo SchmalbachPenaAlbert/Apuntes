8ee82a800487db94feed5bacc85efe13bd65734f
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-02-18 20:03:16 +0100

    Fix style change for the odd rows in the js file for exercise 3

--- lenguajesDeMarcas/UD5/Relacion2/Ejercicio3/js/script.js
+++ lenguajesDeMarcas/UD5/Relacion2/Ejercicio3/js/script.js
@@ -4,6 +4,6 @@ for (let i = 0; i < noticias.length; i++) {
     if (i % 2 == 0) {
         noticias[i].style.backgroundColor = "lightgray"
     } else {
-        noticias[i].style.backgroundColor = "blue"
+        noticias[i].style.color = "blue"
     }
 }
