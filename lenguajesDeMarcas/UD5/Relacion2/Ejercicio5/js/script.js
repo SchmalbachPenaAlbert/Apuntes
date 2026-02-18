b4f7b1ea22cc618730ac50f4f9722220af8ba745
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-02-18 20:30:37 +0100

    Add js file for exercise 5

--- lenguajesDeMarcas/UD5/Relacion2/Ejercicio5/js/script.js
+++ lenguajesDeMarcas/UD5/Relacion2/Ejercicio5/js/script.js
@@ -1,10 +1,10 @@
 let contenedor = document.getElementById("contenedor")
 let nuevoParrafo1 = document.createElement("p")
-nuevoParrafo1.textContent("Descrubren una nueva especie en la selva amazónica")
+nuevoParrafo1.textContent = "Descrubren una nueva especie en la selva amazónica"
 let nuevoParrafo2 = document.createElement("p")
-nuevoParrafo2.textContent("Avances tecnológicos revolucionan la industria de la salud.")
+nuevoParrafo2.textContent = "Avances tecnológicos revolucionan la industria de la salud."
 let nuevoParrafo3 = document.createElement("p")
-nuevoParrafo3.textContent("El mercado financiero experimenta grandes cambios.")
+nuevoParrafo3.textContent = "El mercado financiero experimenta grandes cambios."
 contenedor.appendChild(nuevoParrafo1)
 contenedor.appendChild(nuevoParrafo2)
 contenedor.appendChild(nuevoParrafo3)
