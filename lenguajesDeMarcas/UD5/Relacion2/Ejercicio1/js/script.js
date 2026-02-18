25792b9fb74dffcccf10f0402eb6617be91fde91
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-02-18 19:25:36 +0100

    Add js file for exercise 1

---
+++ lenguajesDeMarcas/UD5/Relacion2/Ejercicio1/js/script.js
@@ -0,0 +1,6 @@
+const titulo = document.getElementById("titulo")
+const subtitulo = document.getElementById("subtitulo")
+window.addEventListener("load", function() {
+    titulo.innerHTML = "Última Hora: Impactantes Noticias"
+    subtitulo.innerHTML = "Mantente informado con nuestras noticias más recientes"
+})
