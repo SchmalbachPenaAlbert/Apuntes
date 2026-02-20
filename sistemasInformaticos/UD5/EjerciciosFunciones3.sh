da81774e367007669bb2965ac6078035cffd47e6
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-02-20 19:09:19 +0100

    Add cambiar_permisos function

--- sistemasInformaticos/UD5/EjerciciosFunciones3.sh
+++ sistemasInformaticos/UD5/EjerciciosFunciones3.sh
@@ -2,7 +2,7 @@
 # Ejercicios funciones Bash 2
 
 crear_directorio() {
-    if [ -d $1 ]
+    if [ -e $1 ]
     then
         echo "Directorio '$1' ya existe"
     else
@@ -26,6 +26,25 @@ buscar_por_extension() {
     echo "$resultado"
 }
 
+cambiar_permisos() {
+    if [ ! -e $2 ] # si no existe...
+    then
+        echo "El directorio no existe!"
+    else
+        if [[ $1 =~ ^[0-7]{3,4}$ ]] # el doble corchete en la condición es para que bash no se lie con los corchetes del regex ([0-7])
+        then
+            chmod $1 $2
+            echo "Permisos concedidos al archivo."
+        else
+            echo "Formato no válido."
+        fi
+    fi
+}
+
+obtener_propietario() {
+    
+}
+
 echo "Ej 1:"
 crear_directorio $1
 echo "Ej 2:"
@@ -33,7 +52,7 @@ buscar_archivos_grandes $1 $2
 echo "Ej 3:"
 buscar_por_extension $1 $2
 echo "Ej 4:"
-cambiar_permisos $1
+cambiar_permisos $1 $2
 echo "Ej 5:"
 obtener_propietario $1
 echo "Ej 6:"
