c63a761dc24c13d72d9b49d5acb00fa77f45dac1
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-02-23 18:22:49 +0100

    Import html file for exercise 1 and add js file

---
+++ lenguajesDeMarcas/UD5/Relacion3/Ejercicio1/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("titulo").textContent = "Historia del Ska-Punk: Álbumes Icónicos"
+document.getElementById("subtitulo").textContent = "Desde los 80 hasta hoy"
+document.getElementById("titulo").style.color = "yellow"
+document.getElementById("subtitulo").style.backgroundColor = "black"
