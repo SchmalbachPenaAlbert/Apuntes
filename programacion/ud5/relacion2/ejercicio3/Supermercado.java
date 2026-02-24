80c3d00f01b4ba604e89ccc44761d782b5e63abe
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-02-24 18:29:31 +0100

    Add Supermercado function

---
+++ programacion/ud5/relacion2/ejercicio3/Supermercado.java
@@ -0,0 +1,33 @@
+package programacion.ud5.relacion2.ejercicio3;
+
+import java.util.ArrayList;
+
+public class Supermercado {
+    ArrayList<Producto> productos;
+    Supermercado() {
+        this.productos = new ArrayList<>();
+    }
+
+    // metodos
+    public void agregarProductos(Producto productos, int cantidad) {
+        
+    }
+    public void venderProductos(int id, int cantidad) {
+        int stockActual;
+        int nuevoStock;
+        for (Producto producto : productos) {
+            if (producto.getId() == id) {
+                stockActual = producto.getCantidadEnStock();
+                nuevoStock = stockActual - cantidad;
+                producto.setCantidadEnStock(nuevoStock);
+            }
+        }
+    }
+    public String mostrarInventario() {
+        String mensaje = "";
+        for (Producto producto : productos) {
+            mensaje += producto.toString() + "\n";
+        }
+        return mensaje;
+    }
+}
