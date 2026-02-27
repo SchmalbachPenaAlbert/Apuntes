b5c6afbc670556b3599a372f8f5800a10afc1ae5
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-02-27 18:56:29 +0100

    Add epractice exercises for SI UD5

---
+++ sistemasInformaticos/UD5/EjerciciosRepaso.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Ejercicios repaso
+
+promedio_tres_numeros() {
+    # control de errores
+    if [ $# -lt 3 ]
+    then
+        echo "No se han introducido valores suficientes!"
+    else
+        # funcionalidad principal
+        promedio=$( echo "($1 + $2 + $3) / $#" | bc -l )
+        echo "El promedio es $promedio."
+    fi
+}
+
+echo "Ej 1:"
+promedio_tres_numeros $1 $2 $3
+
+conversor_de_temperaturas() {
+    temperaturaCelsius=$1
+    temperaturaFarenheit=$( echo "($temperaturaCelsius * 9) / 5 + 32" | bc -l )
+    echo "La temperatura introducida (${temperaturaCelsius} celcius) equivale a ${temperaturaFarenheit} farenheits."
+}
+
+echo "Ej 2:"
+conversor_de_temperaturas $1
+
+tabla_multiplicar() {
+    i=0
+    mensaje=""
+    resultado=0
+    limite=10
+    while [[ $i -lt $limite ]] 
+    do
+        ((i++))
+        resultado=$( echo "$i * $1" | bc -l )
+        mensaje+="${i} x ${1} = ${resultado} | "
+    done
+    echo "$mensaje"
+}
+
+echo "Ej 3:"
+tabla_multiplicar $1
+
+calcular_iva() {
+precioBase=$1
+porcentajeIva=$2
+precioAnadido=$( echo "$precioBase * 0.$porcentajeIva" | bc -l )
+precioTotal=$( echo "$precioBase + $precioAnadido" | bc -l )
+echo "El precio con IVA es $precioTotal"
+}
+
+echo "Ej 4:"
+calcular_iva $1 $2
