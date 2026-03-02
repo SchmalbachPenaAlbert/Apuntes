7dbec1ca4bf4f3e1e9f970fe4c6bbf16c6b4b44f
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-03-02 20:10:48 +0100

    Add class Gestoría with sketch of TODO methods

---
+++ programacion/ud5/relacion3/ejercicio2/Gestoria.java
@@ -0,0 +1,38 @@
+package programacion.ud5.relacion3.ejercicio2;
+
+import java.util.ArrayList;
+
+public class Gestoria {
+    String nombre;
+    String direccion;
+    String telefono;
+    String horario;
+    ArrayList<Cliente> clientes;
+    ArrayList<Tramite> tramites;
+    ArrayList<Expediente> expedientes;
+
+    public void altaCliente(Cliente c) {
+
+    }
+    public void altaTramite(Tramite t) {
+
+    }
+    public void abrirExpediente (...) {
+
+    }
+    public void cambiarEstadoExpediente (...) {
+        
+    }
+    public void marcarExpedientePagado (...) {
+        
+    }
+    public void listarPorEstado (String estado) {
+        
+    }
+    public void listarPendientesDePago () {
+        
+    }
+    public void calcularFacturacionTotal () {
+        
+    }
+}
