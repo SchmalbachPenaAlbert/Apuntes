3e664553599a2251c5239bd431c48493a5fb2bce
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-03-02 22:23:09 +0100

    Fix main file for pt5 pr

--- programacion/ud5/pt5/ASP_PT5.java
+++ programacion/ud5/pt5/ASP_PT5.java
@@ -12,7 +12,7 @@ public class ASP_PT5 {
         int opciónUsuario = 0;
         int cantidadModificar = 0;
 
-        Animal animalPH = null;
+        Animal animal1 = null;
 
         String codigoBuscar = "";
         String mensajeMenu = 
@@ -35,7 +35,7 @@ public class ASP_PT5 {
         // -------------------------------------------------
         
         while (opciónUsuario != OPCIONSALIR) {
-            opciónUsuario = Fnc.pedirNumeroEntero2valores(scanner, 0, OPCIONSALIR, mensajeMenu); //? que es Fnc??
+            opciónUsuario = Funciones.pedirNumeroEntero2valores(scanner, 0, OPCIONSALIR, mensajeMenu); //? que es Funciones??
             scanner.nextLine();
             // enseña todos los animales
             if (opciónUsuario == 1) {
@@ -44,10 +44,10 @@ public class ASP_PT5 {
             // Buscar animal por codigo. hace un toString() del animal
             if (opciónUsuario == 2) {
                 codigoBuscar = scanner.nextLine();
-                animalPH = LoroParque.buscarAnimal(codigoBuscar);
+                animal1 = LoroParque.buscarAnimal(codigoBuscar);
 
-                if (animalPH != null) {
-                    System.out.println(animalPH);
+                if (animal1 != null) {
+                    System.out.println(animal1);
                 } else {
                     System.out.println("No se ha encontrado el animal.");
                 }
@@ -56,7 +56,7 @@ public class ASP_PT5 {
             if (opciónUsuario == 3) {
                 System.out.print("Introduce el código del animal al que quieras agregar ejemplares: ");
                 codigoBuscar = scanner.nextLine();
-                cantidadModificar = Fnc.pedirNumeroEntero1valorMIN(scanner, 0, "Introduce la cantidad que quieres añadir: "); //? que es Fnc??
+                cantidadModificar = Funciones.pedirNumeroEntero1valorMIN(scanner, 0, "Introduce la cantidad que quieres añadir: "); //? que es Funciones??
     
                 if (LoroParque.buscarAnimal(codigoBuscar) != null) {
                     if(LoroParque.agregarEjemplares(codigoBuscar, cantidadModificar)) { //? esto que es? no debería poder funcionar, no?
@@ -72,14 +72,13 @@ public class ASP_PT5 {
             if (opciónUsuario == 4) {
                 System.out.println("Introduce el código del animal al que quieras retirar ejemplares: ");
                 codigoBuscar = scanner.nextLine();
-                cantidadModificar = Fnc.pedirNumeroEntero1valorMIN(scanner, 0, "Introduce la cantidad que quieres añadir: "); //? que es Fnc??
-
+                cantidadModificar = Funciones.pedirNumeroEntero1valorMIN(scanner, 0, "Introduce la cantidad que quieres retirar: "); //? que es Funciones??
                 
                 if(LoroParque.eliminarEjemplares(codigoBuscar, cantidadModificar)) { //? lo mismo que la duda de arriba
                     System.out.println("Se han retirado correctamente los ejemplares");
                 } else {
-                    animalPH = LoroParque.buscarAnimal(codigoBuscar);
-                    if(animalPH.getCantidadEjemplares() - cantidadModificar < 0) {
+                    animal1 = LoroParque.buscarAnimal(codigoBuscar);
+                    if(animal1.getCantidadEjemplares() - cantidadModificar < 0) {
                         System.out.println("No se pueden quedar las cantidades en negatvio");
                     } else {
                         System.out.println("No se han retirado correctamente los ejemplares");
@@ -94,10 +93,10 @@ public class ASP_PT5 {
                 if(LoroParque.eliminarEspecie(codigoBuscar)) { //? lo mismo que la duda de arriba
                     System.out.println("Animal eliminado del zoologico correctamente");
                 } else {
-                    animalPH = LoroParque.buscarAnimal(codigoBuscar);
-                    if (animalPH == null) {
+                    animal1 = LoroParque.buscarAnimal(codigoBuscar);
+                    if (animal1 == null) {
                         System.out.println("No se ha podido eliminar al animal por que no se encontró.");
-                    } else if (animalPH.getCantidadEjemplares() != 0) {
+                    } else if (animal1.getCantidadEjemplares() != 0) {
                         System.out.println("No se puede eliminar un animal con ejemplares disponibles.");
                     } 
                 }
