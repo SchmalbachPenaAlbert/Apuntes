bd8bfb2ff4af49ab72e30a6210b1289291192f15
Albert <schmalbachpenaalbert001.alu@stemgranada.com>
2026-03-25 15:52:34 +0100

    Add country search by name

--- lenguajesDeMarcas/UD6/Countries/script.js
+++ lenguajesDeMarcas/UD6/Countries/script.js
@@ -20,9 +20,53 @@ async function obtenerPais(valorBusqueda) {
 // Función para mostrar los datos del pais en la página
 function mostrarPais(dato) {
     const contenedor = document.getElementById("pais-nombre");
+    let capitales = ""
+    let idiomas = ""
+    let monedas = ""
+    let poblacion = ""
+
+    for (let capital of dato.capital) {
+        capitales += capital + "\n"
+    }
+
+    for (let idioma of Object.values(dato.languages)) {
+        idiomas += idioma + "\n"
+    }
+
+    for (let moneda of Object.values(dato.currencies)) {
+        monedas += moneda.name + " (" + moneda.symbol + ")" + "\n"
+    }
+
+    if (dato.population > 1000000) {
+        poblacion = (dato.population / 1000000).toFixed(2) + " milliones de habitantes"
+    } else if (dato.population > 100000) {
+        poblacion = (dato.population / 100000).toFixed(2) + " mil habitantes"
+    } else {
+        poblacion = dato.population + " habitantes"
+    }
+
+    const nombreComunNativo = Object.values(dato.name.nativeName)[0].common
+
+    const nombreOficialNativo = Object.values(dato.name.nativeName)[0].official
 
     contenedor.innerHTML = `
         <p>${dato.flag}</p>
+        <p><strong>Nombre común:</strong></p>
+        <p>${dato.name.common} (${nombreComunNativo})</p>
+        <p><strong>Nombre oficial:</strong></p>
+        <p>${dato.name.official} (${nombreOficialNativo})</p>
+        <p><strong>Capital:</strong></p>
+        <p>${capitales}</p>
+        <p><strong>Región:</strong></p>
+        <p>${dato.region}</p>
+        <p><strong>Subregión:</strong></p>
+        <p>${dato.subregion}</p>
+        <p><strong>Población:</strong></p>
+        <p>${poblacion}</p>
+        <p><strong>Idiomas:</strong></p>
+        <p>${idiomas}</p>
+        <p><strong>Monedas:</strong></p>
+        <p>${monedas}</p>
     `
 }
 
